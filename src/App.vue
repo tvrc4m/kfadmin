136b798a2dfefc0bbf935f174af5c4a9e056ab5d
刘宏富 <liuhongfu@kanfanews.com>
2018-03-14 17:25:11 +0800

    resolve conflict

--- src/App.vue
+++ src/App.vue
@@ -1,41 +1,4 @@
 <template>
-<<<<<<< HEAD
-    <div id="app">
-        <el-container>
-            <el-container>
-                <el-aside v-if="show">
-                    <slider></slider>
-                </el-aside>
-            </el-container>
-            <el-container>
-                <el-header  v-if="show" height="60px">
-                    <top></top>
-                </el-header>
-                <el-main>
-                    <router-view name="default"></router-view>        
-                </el-main>
-                <el-footer height="40px"></el-footer>
-            </el-container>
-        </el-container>
-    </div>
-</template>
-
-<script>
-    import slider from "./components/navbar/slider.vue"
-    import top from "./components/navbar/header.vue"
-    export default{
-        name: 'app',
-        components:{slider,top},
-        data(){
-            return {
-                show:!!this.$store.state.user.auth_key
-            }
-        }
-    }
-</script>
-<style lang="scss">
-</style>
-=======
 	<div id="app">
 		<router-view></router-view>
 	</div>
@@ -46,4 +9,3 @@
     name: 'App'
   }
 </script>
->>>>>>> 8a7a055b990a75524427781702359240cf9a8698
