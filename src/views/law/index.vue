4ad799d8c640305c57a13242ef98c48b0515e2ec
tvrc4m <weishan@kanfanews.com>
2018-03-15 15:17:04 +0800

    封装keywords和pagination组件

--- src/views/law/index.vue
+++ src/views/law/index.vue
@@ -22,17 +22,21 @@
                 </el-table-column>
             </el-table>
         </el-row>
+        <pagination :total="total" :page_size="page_size" @change="changePage"></pagination>
     </div>
 </template>
 <script>
     import confirm from "@/components/Confirm/dialog"
+    import pagination from "@/components/Pagination/index"
     import {getLawList,delLaw} from "@/api/law"
     export default{
-        components:{confirm},
+        components:{confirm,pagination},
         data(){
             return {
                 laws:[],
-                confirm_id:0
+                confirm_id:0,
+                total:0,
+                page_size:20
             }
         },
         methods:{
@@ -54,16 +58,17 @@
             addLawRule:function(law_id){
                 console.log(law_id)
                 this.$router.push({path:'/law/rule/add',query:{law_id:law_id}})
+            },
+            changePage(page=1){
+                getLawList({page:page}).then(data=>{
+                    this.laws=data.data
+                    this.total=data.total
+                    this.page_size=data.per_page
+                })
             }
         },
         mounted(){
-            getLawList().then(data=>{
-                this.laws=data.data
-                console.log(this.laws)
-            }).catch(error=>{
-                    console.log(error)
-                }
-            )
+            this.changePage()
         }
     }
 </script>
