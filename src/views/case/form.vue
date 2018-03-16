44e6a94906f646ce5a19aa79d76d731f7a24681d
tvrc4m <weishan@kanfanews.com>
2018-03-16 10:45:22 +0800

    重新组织案例tab布局

--- src/views/case/form.vue
+++ src/views/case/form.vue
@@ -1,14 +1,13 @@
 <template>
     <div class="case-container">
-            <el-tabs v-model="tab">
-                <el-tab-pane label="案例内容" name="caseinfo">
-                    <router-view name="default"></router-view>
-                </el-tab-pane>
-                <el-tab-pane label="案例要素" name="casefactor">
-                    <router-view name="case_factor"></router-view>
-                </el-tab-pane>
-            </el-tabs>
-        
+        <el-tabs v-model="tab" @tab-click="tabClicked">
+            <el-tab-pane label="案例内容" name="caseinfo">
+                <router-view name="case_info"></router-view>
+            </el-tab-pane>
+            <el-tab-pane label="案例要素" name="casefactor">
+                <router-view name="case_factor"></router-view>
+            </el-tab-pane>
+        </el-tabs>
     </div>
 </template>
 <script>
@@ -17,16 +16,13 @@
             return {
                 tab:"caseinfo"
             }
+        },
+        methods:{
+            tabClicked(tab){
+                if(tab.name=='casefactor'){
+                    // this.$router.push({path:"/case/add/factor"})
+                }
+            }
         }
     }
 </script>
-<style lang="scss" scoped>
-    .case-container{
-        .el-input,input[type=text],.el-textarea,textarea{
-            width:500px;
-        }
-        textarea{
-            height:100px;
-        }
-    }
-</style>
